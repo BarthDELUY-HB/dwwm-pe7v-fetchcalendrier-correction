927aef1663fa0aa4f0aa607179e152039b43cc1e
Barth DELUY <barth@barthdeluy.me>
2023-08-29 16:28:39 +0200

    gestion des onglets dans le main.js

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 import { fetchHolidays,createCalendar } from "./calendar.func.js";
 
+function manageCalendar()
+{
+    const calendrier = createCalendar();
+    console.log(calendrier);
+    document.querySelector('#section-calendrier').innerHTML = calendrier.innerHTML;
+    fetchHolidays();
+}
+
+function hideAllContentSections()
+{
+    let sections = document.querySelectorAll('.contentSection');
+    let i = 0;
+    while (i<sections.length) {
+        sections[i].classList.add('hidden');
+        i++;
+    }
+}
+
+function displayContentSection(sectionId)
+{
+    let selector = '#' + sectionId;
+    document.querySelector(selector).classList.remove('hidden');
+}
+
+function attachMenuEvents()
+{
+    let links = document.querySelectorAll('.menuLink');
+    let i = 0;
+    while (i<links.length) {
+        links[i].addEventListener('click', function (event) {
+            event.preventDefault();
+            hideAllContentSections();
+            const idElements = event.target.id.split('-');
+            let sectionId = 'section-' + idElements[1];
+            displayContentSection(sectionId);
+            return false;
+        });
+        i++;
+    }
+}
+
 window.addEventListener(
     'DOMContentLoaded',
     function (event) {
-        const calendrier = createCalendar();
-        console.log(calendrier);
-        document.querySelector('#section-calendrier').innerHTML = calendrier.innerHTML;
-        fetchHolidays();
+        manageCalendar();
+        attachMenuEvents();
     });
